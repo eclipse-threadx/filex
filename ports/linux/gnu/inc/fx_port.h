e86ca8981387e74051371b051bc8bb0d2bdc27a5
Frédéric Desbiens <frederic.desbiens@eclipse-foundation.org>
2026-03-05 14:39:17 +0100

    Updated port version number strings

--- ports/linux/gnu/inc/fx_port.h
+++ ports/linux/gnu/inc/fx_port.h
@@ -331,8 +331,7 @@ extern VOID fault_tolerant_apply_log_callback(struct FX_MEDIA_STRUCT *media_ptr,
 
 #ifdef FX_SYSTEM_INIT
 CHAR                            _fx_version_id[] =
-                                    "Copyright (c) 2024 Microsoft Corporation.  *  FileX Linux/GCC Version 6.4.1 *";
- * Copyright (c) 2026-present Eclipse ThreadX contributors
+                                    "(c) 2024 Microsoft Corp. (c) 2026-present Eclipse ThreadX contributors. * FileX Linux/GCC Version 6.5.0.202601 *";
 #else
 extern  CHAR                    _fx_version_id[];
 #endif
